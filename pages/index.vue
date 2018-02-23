26803a9000d138ceecf3b60cd4f9344fae1eb521
MattDev84 <MattDev84@outlook.com>
2018-02-23 13:53:26 +0000

    Basics set up

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h1>Welcome to the App</h1>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
